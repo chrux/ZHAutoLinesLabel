068b583c301822155afda07a830127a1ffa69451
Christian Torres <chtorrez@gmail.com>
2015-10-06 14:01:37 -0600

    Fixing init(coder:) method for Swift 2.0.

--- ZHAutoLinesLabel.swift
+++ ZHAutoLinesLabel.swift
@@ -24,7 +24,7 @@ class ZHAutoLinesLabel: UILabel {
         setup()
     }
 
-    required init(coder aDecoder: NSCoder) {
+    required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setup()
     }
